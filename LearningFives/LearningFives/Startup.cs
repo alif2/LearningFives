0a08d8d4d071dc08f71945d2a6da5eb7054292d2
Adam Lif <adamlnk@neb.rr.com>
2016-01-10 02:10:11 -0600

    Project init

---
+++ LearningFives/LearningFives/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningFives.Startup))]
+namespace LearningFives
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
