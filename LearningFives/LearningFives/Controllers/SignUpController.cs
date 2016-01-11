9cd531c34261202b2d33eca6ca18363db08bdb04
Adam Lif <adamlnk@neb.rr.com>
2016-01-10 19:15:20 -0600

    Create student sign up form

---
+++ LearningFives/LearningFives/Controllers/SignUpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningFives.Controllers
+{
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        public ActionResult Coach()
+        {
+            return View();
+        }
+
+        // GET: SignUp
+        public ActionResult Student()
+        {
+            return View();
+        }
+    }
+}
