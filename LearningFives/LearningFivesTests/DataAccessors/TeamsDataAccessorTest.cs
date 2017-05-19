cc96fd9bd383cd161a4ce852d1f06505aef553d0
Adam Lif <adamlnk@neb.rr.com>
2017-05-19 18:07:50 -0500

    Add L5 tests proj

---
+++ LearningFives/LearningFivesTests/DataAccessors/TeamsDataAccessorTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using DataAccessors;
+using DataAccessors.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LearningFivesTests.DataAccessors
+{
+    [TestClass]
+    public class TeamsDataAccessorTest
+    {
+        private TeamsDataAccessor _teamsDataAccessor;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _teamsDataAccessor = new TeamsDataAccessor();
+            Assert.IsNotNull(_teamsDataAccessor);
+        }
+
+        [TestMethod]
+        public async Task GetAllStudentsAsyncTest()
+        {
+            var filter = new StudentFilterDM
+            {
+                RankTier = null,
+                Server = null,
+                StudentStatus = -1
+            };
+
+            var students = await _teamsDataAccessor.GetAllStudentsAsync(filter);
+            foreach (var student in students)
+            {
+                Assert.IsTrue(student.StudentSignUpID >= 0);
+                Assert.IsTrue(student.StudentStatus >= 0 && student.StudentStatus <= 1);
+                Assert.IsTrue(student.SummonerInfoID >= 0);
+                Assert.IsTrue(student.AvailabilityInfoID >= 0);
+                Assert.IsFalse(string.IsNullOrEmpty(student.Languages));
+                Assert.IsFalse(string.IsNullOrEmpty(student.PairedPlayers));
+                Assert.IsFalse(string.IsNullOrEmpty(student.PlayerStrengths));
+                Assert.IsFalse(string.IsNullOrEmpty(student.PlayerImprovement));
+                Assert.IsFalse(string.IsNullOrEmpty(student.PlayerExpectations));
+                Assert.IsTrue(student.CommitmentID >= 0);
+                Assert.IsTrue(student.ToxicID >= 0);
+                Assert.IsFalse(string.IsNullOrEmpty(student.SummonerInfo.SummonerName));
+                Assert.IsFalse(string.IsNullOrEmpty(student.SummonerInfo.ServerName));
+                Assert.IsTrue(student.SummonerInfo.Age > 0);
+                Assert.IsFalse(string.IsNullOrEmpty(student.SummonerInfo.Email));
+            }
+        }
+    }
+}
---
+++ LearningFives/LearningFivesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LearningFivesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearningFivesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c51bfd5-23da-4df5-900c-c06bceb9f720")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
